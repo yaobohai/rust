8de4b3836ef9adc4a56fc905adf136a3d25e5feb
无敌的博海 <424079134@qq.com>
2023-08-17 22:25:01 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## rust
+
+Install rust
+
+```
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+```
+
+New rust Project
+
+```
+$ cargo new <project_name>
+```
+
---
+++ src/main.rs
@@ -0,0 +1,10 @@
+fn main() {    
+    let status="😅";
+    let user_name="博海";
+
+    println!("你好啊,{}{} ",user_name,status);
+
+    for i in 1..=5 {
+        println!("这是第{}次 呵呵{}!",i,status)
+    }
+}
