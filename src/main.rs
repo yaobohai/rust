55c26aef991ff70f678b3adf8946e037808ad706
姚波海 <yaobohai@hd123.com>
2023-08-18 10:40:34 +0800

    rust

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,13 @@ fn main() {
     let user_name="博海";
     let user_status="😅";
 
-    println!("你好啊,{}{} ",user_name,status);
+    println!("你好啊,{}{} ",user_name,user_status);
     println!("------------------------------");
 
     // 调用say函数返回值
     let beybey = say();
 
     for i in 1..=5 {
-        println!("这是第{}次 呵呵{} {}!",i,status,beybey)
+        println!("这是第{}次 呵呵{} {}!",i,user_status,beybey)
     }
 }
